2fb1e5bd7198150e90a227aa57bf928314847998
Johan Nicolin <johan.nicolin@gmail.com>
2025-05-14 16:00:03 +0200

    added two Django apps reeds and comments

--- reedproject/settings/base.py
+++ reedproject/settings/base.py
@@ -25,7 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'posts',
+    'reeds',
+    'comments',
 ]
 
 MIDDLEWARE = [
--- reedproject/urls.py
+++ reedproject/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import my_blog
+from posts.views import my_post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', my_blog, name='blog'),
+    path('posts/', my_post, name='posts'),
 ]
