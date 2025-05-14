2c180b2743e3fb53aa9427cba3ad9b27ac163328
Johan Nicolin <johan.nicolin@gmail.com>
2025-05-15 00:55:47 +0200

    mock views for app posts added

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from .forms import PostForm
 
-# Create your views here.
-def my_post(request):
-    return HttpResponse("Hello World")
+# READ: List
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_on')
+    return render(request, 'posts/post_list.html', {'posts': posts})
+
+# READ: Detail
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'posts/post_detail.html', {'post': post})
+
+# CREATE
+def post_create(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user 
+        post.save()
+        return redirect('post_list')
+    return render(request, 'posts/post_form.html', {'form': form})
+
+# UPDATE
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('post_detail', pk=pk)
+    return render(request, 'posts/post_form.html', {'form': form})
+
+# DELETE
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'posts/post_confirm_delete.html', {'post': post})
--- reedproject/urls.py
+++ reedproject/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from posts.views import my_post
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', my_post, name='posts'),
+    path('posts/', include("posts.urls"), name='posts-urls'),
 ]
