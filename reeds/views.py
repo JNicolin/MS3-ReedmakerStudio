b23b7e1f027c55eb3fc2270bc17f99b09dfb92c6
Johan Nicolin <johan.nicolin@gmail.com>
2025-05-16 00:59:57 +0200

    Added templates for app Reed

--- reeds/views.py
+++ reeds/views.py
@@ -1,26 +1,43 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views import generic
 from .models import Reed
 from .forms import ReedForm
+from comments.forms import CommentForm
 
 # CRUD views for Reed
 
 # READ: List
 def reed_list(request):
     posts = Reed.objects.all().order_by('-created_on')
-    return render(request, 'reeds/reed_list.html', {'reed_list': reeds})
+    return render(request, 'reeds/reed_list.html', {'reeds': reeds})
 
 # READ: Detail
 def reed_detail(request, pk):
-    post = get_object_or_404(Reed, pk=pk)
-    return render(request, 'reeds/reed_detail.html', {'reed': reed})
+    reed = get_object_or_404(Reed, pk=pk)
+    comments = reed.comments.all()
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.content_object = reed
+            comment.save()
+            return redirect('reed_detail', pk=pk)
+    else:
+        form = CommentForm()
+
+    return render(request, 'reeds/reed_detail.html', {
+        'reed': reed,
+        'comments': comments,
+        'form': form
+    })
 
 # CREATE
 def reed_create(request):
     form = ReedForm(request.POST or None)
     if form.is_valid():
         reed = form.save(commit=False)
-        reed.author = request.user 
+        reed.item_creator = request.user 
         reed.save()
         return redirect('reed_list')
     return render(request, 'reeds/reed_form.html', {'form': form})
@@ -28,16 +45,16 @@ def reed_create(request):
 # UPDATE
 def reed_update(request, pk):
     reed = get_object_or_404(Reed, pk=pk)
-    reed = ReedForm(request.POST or None, instance=reed)
+    form = ReedForm(request.POST or None, instance=reed)
     if form.is_valid():
         form.save()
-        return redirect('post_detail', pk=pk)
-    return render(request, 'posts/post_form.html', {'form': form})
+        return redirect('reed_detail', pk=pk)
+    return render(request, 'reeds/reed_form.html', {'form': form})
 
 # DELETE
-def post_delete(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+def reed_delete(request, pk):
+    reed = get_object_or_404(Reed, pk=pk)
     if request.method == 'POST':
-        post.delete()
-        return redirect('post_list')
-    return render(request, 'posts/post_confirm_delete.html', {'post': post})
+        reed.delete()
+        return redirect('reed_list')
+    return render(request, 'posts/post_confirm_delete.html', {'reed': reed})
