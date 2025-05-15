207594ff4f61ec810055ae0cdc2be268da06ddb9
Johan Nicolin <johan.nicolin@gmail.com>
2025-05-16 01:10:07 +0200

    Added comments/forms.py

--- comments/forms.py
+++ comments/forms.py
@@ -4,5 +4,16 @@ from .models import Comment
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['title', 'body', 'author', 'approved']
-        
+        fields = ['title', 'body']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'placeholder': 'Title'
+            }),
+            'body': forms.Textarea(attrs={
+                'rows': 3, 
+                'placeholder': 'Write a comment...'})
+        }
+        labels = {
+            'title': '',
+            'body': ''
+        }
