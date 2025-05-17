2b657746217129ed857035f31fe66a60536e01a9
Johan Nicolin <johan.nicolin@gmail.com>
2025-05-17 13:22:44 +0200

    Updated styling of reed detail  html

--- comments/forms.py
+++ comments/forms.py
@@ -7,9 +7,11 @@ class CommentForm(forms.ModelForm):
         fields = ['title', 'body']
         widgets = {
             'title': forms.TextInput(attrs={
+                'class': 'form-control',
                 'placeholder': 'Title'
             }),
             'body': forms.Textarea(attrs={
+                'class': 'form-control',
                 'rows': 3, 
                 'placeholder': 'Write a comment...'})
         }
